faff9491a09aba7a854aff3c6662e18dc5712ce6
MaxZP <maxsol.zp@gmail.com>
2018-01-21 04:58:05 +0200

    temp-1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="App-header">
+            <h1>Its a first React app - Header</h1>
+        </header>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+        <footer className="App-footer">
+            <h5>Its a first React app - Footer</h5>
+        </footer>
+    );
+  }
+}
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to my ferst React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer />
       </div>
     );
   }
