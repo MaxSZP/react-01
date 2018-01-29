f32660fdcbe508b9a4ac71de0f7e6c011d56ef4a
MaxZP <maxsol.zp@gmail.com>
2018-01-29 16:33:26 +0200

    temp-18-01-28

--- src/App.js
+++ src/App.js
@@ -24,11 +24,117 @@ class Footer extends Component {
 }
 
 
+
+
+
+
+class OneInput  extends Component {
+  onBtnClickHandler() {
+    //alert(ReactDOM.findDOMNode(this.refs.myTestInput).value);
+    alert('Kliknuli');
+  }
+  render() {
+    return (
+      <div>
+        <input
+          className='one-input'
+          defaultValue=''
+          placeholder='введите значение'
+          ref='myTestInput'
+        />
+        <button onClick={this.onBtnClickHandler}>Добавить значение вар-1</button>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+class TestInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {myValue: ''};
+  }
+  onChangeHandler(e) {
+    this.setState({myValue: e.target.value})
+  }
+  onBtnClickHandler() {
+    alert(this.state.myValue);
+  }
+  render() {
+    return (
+      <div>
+        <input
+          className='test-input'
+          value={this.state.myValue}
+          onChange={this.onChangeHandler}
+          placeholder='введите значение'
+        />
+        <button onClick={this.onBtnClickHandler}>Показать alert</button>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  render() {
+    return (
+      //...
+      <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
+      //...
+    );
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <OneInput />
+        <TestInput />
+        <MyComponent />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
